9c1587801ee7d649657c5e764ed3cd302879f11f
Boris Ts <cborka777@yandex.ru>
2023-07-10 00:33:59 +0700

    230709 sunday

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
 import express from 'express';
 export const app = express();
 
-const PORT = process.env.PORT || 3000
+import fs from 'fs';
+import path from 'path';
 
+const PORT = process.env.PORT || 3000
 console.log('PORT = ' + PORT);
 
+
+
+app.use(express.static('public'));
+
 import {router as indexRouter} from './routes/index.js'; 
+
+// view engine setup
+//app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'views');
+app.set('view engine', 'hbs'); 
 
 
 
 
 app.use(indexRouter);
 
+// app.use(function(err, req, res, next) {
+//   console.log('err.stack');
+//   console.error(err.stack);
+//   res.status(500).send('Something broke!');
+// });
 
-app.listen(PORT,  function () {
+
+
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!' + req.url);
+  //console.log(err);
+});
+
+
+app.listen(PORT, () => {
   console.log('Server started at port ' + PORT);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,59 @@
 import  express  from "express";
 export const router = express.Router();
 
-
-const ip = function(req, res) {
+const ip = function(req, res, next) {
   let s = "Your IP Addresss is: " + req.socket.localAddress + '<br>';
-  s += 'Remote IP Address is: ' + req.socket.remoteAddress;
+  s += 'Remote IP Address is: ' + req.socket.remoteAddress + '<br>';
+  s += 'req.ip: ' + req.ip + '<br>';
+  s += 'import.meta.url: ' + import.meta.url + '<br>';
+  s += 'process.cwd(): ' + process.cwd() + '<br>';
+  s += '<a href="/">Home</a><br>';
+  //next('route');
   res.send(s);
 }
 
-router.get('/', ip);
+const ip2 = function(req, res) {
+  //res.send('next route');
+  res.redirect('/hbs')
+  //res.render('regular');
+}
+
+
+const hbs = function(req, res) {
+  res.render('index', {title: "Veda"});
+  //res.render('regular');
+}
+
+const applocals = function(req, res) {
+  res.json(req.app.locals);
+  //res.render('regular');
+}
+
+router.get('/ip',  ip);
+router.get('/ip', ip2);
+router.get('/ip2', ip2);
+router.get('/hbs', hbs);
+router.get('/applocals', applocals);
+
+
+
+//
+router.get('/sendFile', function (req, res, next) {
+  var options = {
+    root: process.cwd(),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+
+  var fileName = 'public/favicon.bmp';
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err)
+    } else {
+      console.log('Sent:', fileName)
+    }
+  })
+})
