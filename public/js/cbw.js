4d169e5ae6f65b8d8b116b0ddef236fc1dc852a3
Boris Ts <cborka777@yandex.ru>
2024-05-21 22:19:01 +0700

    240521-22

--- public/js/cbw.js
+++ public/js/cbw.js
@@ -1,5 +1,7 @@
 
-async function fetch2(url, params) {
+
+// Fetch на клиенте (frontend)
+async function myFetch2(url, params) {
 
   if (params) {
     response = await fetch(url, {
@@ -24,27 +26,5 @@ async function fetch2(url, params) {
     return 'Ошибка getContent' + response.statusText;
   }
 
-  //let response = await fetch('http://127.0.0.1:3000/user/isLoginFree', {
-  // let response = await fetch('/user/isLoginFree', {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json'},
-    //headers: { 'Content-Type': 'text/plain'},
-    //bbbbb: 'xxx',
-    // body: JSON.stringify(user)
-//    body: 'mode=22' 
-  //  body: 'modemode'
-  // });
-  //alert(response.message);
-  
-  
-     // let response = await fetch('/user/isLoginFree');
-     //let txt = await response.text();
-     alert(await response.text());
-      // if(response.ok) {
-      // let txt = await response.text();
-      // alert(txt);
-      // } else {
-      // alert('Ошибка HTTP: ' + response.statusText);
-      // }
-  }
+}
   
